039caf52d8a185d8047889d8f4bc2dfec0b4caff
Dharmendra Awasthi <dharmendrakawasthi@gmail.com>
2020-04-06 00:43:58 +0530

    Updated Names

--- src/main/java/com/techendear/concurrency/advanced/BasicExecutorServiceExample.java
+++ src/main/java/com/techendear/concurrency/advanced/BasicExecutorServiceExample.java
@@ -3,7 +3,7 @@ package com.techendear.concurrency.advanced;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class SyncLocPattern {
+public class BasicExecutorServiceExample {
 
 	public static void main(String[] args) {
 
